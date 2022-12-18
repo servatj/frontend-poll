76351150763842d275d846a8dd3eeb1c31f8919b
servatj <jservatlorca@gmail.com>
2022-12-18 16:05:22 +0400

    fix linting2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Audio } from 'react-loader-spinner';
+import { Audio } from "react-loader-spinner";
 
 import { useEffect, useState } from "react";
 import { useForm, Controller } from "react-hook-form";
@@ -19,7 +19,7 @@ import DenseTable from "./components/Table";
 import ResponsiveAppBar from "./components/Header";
 
 import useWeb3 from "./hooks/useWeb3";
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
 const theme = createTheme({
   palette: {
@@ -46,65 +46,82 @@ const App = () => {
   const [bets, setBets] = useState([]);
   const [isWallet, setIsWallet] = useState(false);
   const [rows, setRows] = useState([]);
-  const [isLoading, setLoading] =useState(false);
-  const [lastTransaction, setLastTransaction] =useState(false);
-  
-  async function addBet(account, usdcContract, betPoolContract, betPoolAddress, nickname, scoreA, scoreB) {
-   try {
-     console.log('bet pool contract when addbet', betPoolContract)
-     const allowance = await usdcContract.allowance(account, betPoolAddress)
-     console.log(allowance, ethers.utils.formatEther(allowance))
-     if(ethers.utils.formatEther(allowance) === '0.0') {
-       await usdcContract.approve(betPoolAddress, ethers.utils.parseEther('1000'))
-     }
-     const transaction = await betPoolContract.bet(nickname, scoreA, scoreB);
-     const tx = await transaction.wait();
-     setLoading(false);
-     setLastTransaction(true);
-  
-   } catch(error) {
+  const [isLoading, setLoading] = useState(false);
+  const [lastTransaction, setLastTransaction] = useState(false);
+
+  async function addBet(
+    account,
+    usdcContract,
+    betPoolContract,
+    betPoolAddress,
+    nickname,
+    scoreA,
+    scoreB
+  ) {
+    try {
+      console.log("bet pool contract when addbet", betPoolContract);
+      const allowance = await usdcContract.allowance(account, betPoolAddress);
+      console.log(allowance, ethers.utils.formatEther(allowance));
+      if (ethers.utils.formatEther(allowance) === "0.0") {
+        await usdcContract.approve(
+          betPoolAddress,
+          ethers.utils.parseEther("1000")
+        );
+      }
+      const transaction = await betPoolContract.bet(nickname, scoreA, scoreB);
+      await transaction.wait();
+      setLoading(false);
+      setLastTransaction(true);
+    } catch (error) {
       console.log(error);
       setLoading(false);
-   }
-  } 
-  
-  const { active, account, connect, disconnect, isMetamaskInstalled, signer, provider, usdcContract, betPoolContract, betPoolAddress } =
-    useWeb3();
+    }
+  }
+
+  const {
+    active,
+    account,
+    connect,
+    disconnect,
+    isMetamaskInstalled,
+    usdcContract,
+    betPoolContract,
+    betPoolAddress,
+  } = useWeb3();
   const wallet = { active, account, connect, disconnect, isMetamaskInstalled };
-  const { handleSubmit, reset, control } = useForm();
-  
+  const { handleSubmit, control } = useForm();
+
   const onSubmit = (data) => {
-    if(!active) {
-      alert('Please Connect your wallet');
+    if (!active) {
+      alert("Please Connect your wallet");
       return;
     }
     setLastTransaction(false);
     setLoading(true);
-    addBet(account, usdcContract, betPoolContract, betPoolAddress, data.nickname, data.ScoreA, data.ScoreB);
-    setRows([...rows, createData(data.nickname, 1, [data.ScoreA, data.ScoreB], account)]);
+    addBet(
+      account,
+      usdcContract,
+      betPoolContract,
+      betPoolAddress,
+      data.nickname,
+      data.ScoreA,
+      data.ScoreB
+    );
+    setRows([
+      ...rows,
+      createData(data.nickname, 1, [data.ScoreA, data.ScoreB], account),
+    ]);
   };
 
-  async function getData(betPoolContract) {
-    try {
-      const bets = await getBets(betPoolContract);
-      const rows = [];
-      for(let i = 1; i <= bets; i++) {
-        const data = await betPoolContract.getBetData(i);
-        rows.push(createData(data[0], data[2], data[3], data[1]))
-      }
-      return setRows(rows);
-    } catch(error) {
-     console.log('error getting number of data', error)
-    }
-  } 
+
   async function getBets(betPoolContract) {
     try {
       const bets = await betPoolContract.getBets();
       return bets.toString();
-    } catch(error) {
-     console.log('error getting number of bets', error)
+    } catch (error) {
+      console.log("error getting number of bets", error);
     }
-  } 
+  }
 
   useEffect(() => {
     // check if metamask is installed
@@ -118,14 +135,34 @@ const App = () => {
 
   // use effect to get bets
   useEffect(() => {
-    if(active && betPoolContract) {
+    if (active && betPoolContract) {
       getBets(betPoolContract).then((bets) => {
-        console.log(bets)
+        console.log(bets);
         setBets(bets);
-      })
-      getData(betPoolContract)
+      });
     }
-  }, [active])
+  }, [active, betPoolContract]);
+
+  // use effect to get bets
+  useEffect(() => {
+    async function getData(betPoolContract) {
+      try {
+        const bets = await getBets(betPoolContract);
+        const rows = [];
+        for (let i = 1; i <= bets; i++) {
+          const data = await betPoolContract.getBetData(i);
+          rows.push(createData(data[0], data[2], data[3], data[1]));
+        }
+        return setRows(rows);
+      } catch (error) {
+        console.log("error getting number of data", error);
+      }
+    }
+
+    if (active && betPoolContract) {
+      getData(betPoolContract);
+    }
+  }, [active, betPoolContract]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -145,8 +182,13 @@ const App = () => {
               <h1> 🇦🇷 ARGENTINA 0 vs 🇫🇷 FRANCE 0 </h1>
             </Typography>
           </Box>
-          <Box display="flex" justifyContent='center' sx={{ my: 4 }}>
-              {isLoading && <><Audio wrapperStyle color="black" height={80} width={80} /> Loading ...</>}
+          <Box display="flex" justifyContent="center" sx={{ my: 4 }}>
+            {isLoading && (
+              <>
+                <Audio wrapperStyle color="black" height={80} width={80} />{" "}
+                Loading ...
+              </>
+            )}
           </Box>
           <CssBaseline />
           <Box
@@ -158,8 +200,17 @@ const App = () => {
             }}
           >
             <Typography component="h1" variant="h5">
-              ⚽️ Bets on the game ⚽️ { bets === '0' ? <strong> No bets yet </strong> : <strong> {Number(bets)} bets </strong> }
-              {lastTransaction && <p style={ {color : 'green' }}>New Bet Set --> show transaction</p>}
+              ⚽️ Bets on the game ⚽️{" "}
+              {bets === "0" ? (
+                <strong> No bets yet </strong>
+              ) : (
+                <strong> {Number(bets)} bets </strong>
+              )}
+              {lastTransaction && (
+                <p style={{ color: "green" }}>
+                  New Bet Set --> show transaction
+                </p>
+              )}
             </Typography>
             <Box
               component="form"
@@ -229,10 +280,9 @@ const App = () => {
                   ADD BET
                 </Button>
               </form>
-              
             </Box>
           </Box>
-          <DenseTable rows={rows} sx={{ mb: 20 }}/>
+          <DenseTable rows={rows} sx={{ mb: 20 }} />
         </Container>
       )}
     </ThemeProvider>
